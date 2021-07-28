477fe76a4f983ec3ecd4d690ed8d51f9e08c183b
SebasJak <sebastian.lukich.aste@alumnos.uta.cl>
2021-07-27 22:24:02 -0400

    Más componentes

--- index_remastered.php
+++ index_remastered.php
@@ -1,3 +1,12 @@
+<?php
+        include "conexion.php";
+
+        $sql = "SELECT * FROM  hellkitchen.recetas";
+        $result = $conn->query($sql);
+        
+        if($result->num_rows > 0){
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,22 +18,49 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Hell's Kitchen</title>
-</head>
-
+  </head>
   <body>
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a href="#" class="navbar-brand page-scroll">Hell's Kitchen</a>
+    <h1>Hello, world!</h1>
+    <!--
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+        </ul>
       </div>
-      <ul class="nav navbar-nav">
-        <li class="active page-scroll"><a href="index.html">Inicio</a></li>
-        <li class="page-scroll"><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
-      </ul>
+    </nav>
+    -->
+    <!-- Sección de espacio blanco 1 -->
+    <div style="height: 80px;"></div>
+
+    <div class="container">
+      <div class="row">
+        <div class="thumbnail text-center">
+          <a href="ver_r.php  id="<?php $row['nombre_receta']?>">
+            <img class="img-responsive" src="<?php echo $row['imagen'] ?>" alt="<?php echo $row['nombre_receta'] ?>">
+            <div class="caption"><?php echo $row['nombre_receta'] ?></div>
+          </a>
+        </div>
+      </div>
     </div>
-  </nav>
 
+    
 
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
   </body>
 </html>
