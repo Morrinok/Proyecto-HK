b91bddd68f7d6c8b9f2ac6a9856e67e88afa8534
SebasJak <sebastian.lukich.aste@alumnos.uta.cl>
2021-07-27 23:30:27 -0400

    Menús

--- index_remastered.php
+++ index_remastered.php
@@ -34,14 +34,14 @@
   </nav>
   <div class="container">
     <!-- Sección con las categorías -->
-    <div class="row">
+    <div class="row" style="background-color: yellow;">
       <h1 class="text-center"> Recetas </h1>
       <?php while($row = $result->fetch_assoc()){
       ?>
       <div class="col-sm-2">
         <div class="thumbnail text-center">
           <a href="ver_r.php  id="<?php $row['nombre_receta']?>">
-            <img class="img-responsive" src="<?php echo $row['imagen'] ?>" alt="<?php echo $row['nombre_receta'] ?>">
+            <img style="width: 160px; height: 90px;" src="<?php echo $row['imagen'] ?>" alt="<?php echo $row['nombre_receta'] ?>">
             <div class="caption"><?php echo $row['nombre_receta'] ?></div>
           </a>
         </div>
@@ -51,6 +51,50 @@
       ?>
     </div>
   </div>
+<!-- 
+  <div class="container-fluid">
+      -->
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="panel-group">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title text-center">
+                <a data-toggle="collapse" href="#collapse1" >Recetas más preparadas</a>
+              </h4>
+            </div>
+            <div id="collapse1" class="panel-collapse collapse">
+              <ul class="list-group">
+                <li class="list-group-item">Porotos a la chilena</li>
+                <li class="list-group-item">Arroz a la cubana</li>
+                <li class="list-group-item">Papa a la huancaína</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="panel-group">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title text-center">
+              <a data-toggle="collapse" href="#collapse2">Recetas esta semana</a>
+            </h4>
+          </div>
+          <div id="collapse2" class="panel-collapse collapse">
+            <ul class="list-group">
+              <li class="list-group-item">Ají de gallina</li>
+              <li class="list-group-item">Ceviche de pulpo</li>
+              <li class="list-group-item">Bistec a lo pobre</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--
+  </div>
+      -->
 
 </body>
 </html>
