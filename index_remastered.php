cd611ce05fd899e96c6716c232a8545792b42c69
SebasJak <sebastian.lukich.aste@alumnos.uta.cl>
2021-07-27 22:02:07 -0400

    Navbar 1
    Navbar 1er intento

--- index_remastered.php
+++ index_remastered.php
@@ -1,12 +1,3 @@
-<?php
-        include "conexion.php";
-
-        $sql = "SELECT * FROM  hellkitchen.recetas";
-        $result = $conn->query($sql);
-        
-        if($result->num_rows > 0){
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,20 +8,23 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
-  </head>
+    <title>Hell's Kitchen</title>
+</head>
+
   <body>
-    <h1>Hello, world!</h1>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a href="#" class="navbar-brand page-scroll">Hell's Kitchen</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active page-scroll"><a href="index.html">Inicio</a></li>
+        <li class="page-scroll"><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
+      </ul>
+    </div>
+  </nav>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
