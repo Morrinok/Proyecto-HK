1669ff5405188f16bbcddbfe41d6fa72adcb8ba3
Geril-pls <85701958+Geril-pls@users.noreply.github.com>
2021-07-28 21:04:42 -0400

    cambio_navbar vista_r_2

--- ver_r_2.php
+++ ver_r_2.php
@@ -19,6 +19,7 @@
     <!-- nav bar style-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style_r.css" media="screen" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 </head>
 <body>
     <div class="cont_principal">
